3a684fe1e32b056770abc1f75f85ff0f83fa4249
genderson97 <gporrasm@ucatolica.ac.cr>
2016-06-24 20:16:54 -0600

    avance escrito y clase palabras

--- ahorcado/src/ahorcado/Ahorcado.java
+++ ahorcado/src/ahorcado/Ahorcado.java
@@ -5,6 +5,9 @@
  */
 package ahorcado;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author gende
@@ -15,9 +18,32 @@ public class Ahorcado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    
+                Scanner scanner = new Scanner(System.in);
+        String [] listaPalabras = {"avion","barco","computadora","dinosaurio","elefante","foca","gato","hielo","iguana","jabon"};
         
-    }
-    
+        //Elegimos un numero random, que sera el numero de palabra que legiremos de la lista de palabras
+        int numeroRandom = 1 + new Random().nextInt(listaPalabras.length);
+        
+        String palabraEscogida = listaPalabras[numeroRandom];//Guardamos la palabra escogida por la pc en una variable
+        System.out.println("Esta palabra tiene "+palabraEscogida.length()+" letras");
+        String[] palabra = new String[palabraEscogida.length()];//si se llena todos los espacios de este array es que ha ganado el juego
+        imprimirPalabra(palabra);//Mostramos las rayitas
+        int vidas = 11;//Contador de vidas
+        
+        
+        
+        
+        
+        
+        
+
 }
+
+    private static void imprimirPalabra(String[] palabra) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+     
+    
+  }
+    
+
--- ahorcado/src/ahorcado/palabras.java
+++ ahorcado/src/ahorcado/palabras.java
@@ -5,37 +5,122 @@
  */
 package ahorcado;
 
+import java.util.Random;
+
 /**
  *
  * @author gende
  */
 public class palabras {
-    private String[] arreglo;
-    private String elegida;
-    private String[] palabraFormada;
-
-    public palabras() 
-    {
-    arreglo = new String[10];
-    arreglo[0] = "teleferico";
-    arreglo[1] = "coche";
-    arreglo[2] = "zapato";     
-    arreglo[3] = "terremoto";
-    arreglo[4] = "electricidad";
-    arreglo[5] = "velocidad";
-    arreglo[6] = "computador";       
-    arreglo[7] = "televisor";
-    arreglo[8] = "abanico";
-    arreglo[9] = "jirafa";  
-    elegida = escogerPalabra();
-    palabraFormada = new String[elegida.length()];
-    llenarPalabraFormada();
-    
+private String[] listapalabras;        
+private String escogerpalabra;
+private String palabraEscogida; ;
+private int numeroRandom = 1+ new Random().nextInt(listapalabras.length);    
+//contador vacio
+    public palabras() {
+    String[] listaPalabras = null;
+        this.palabraEscogida = listaPalabras[numeroRandom];
+        this.listapalabras = new String[]{"teleferico", "televisor", "computador", "coche", "antropologia", "rusia", "hungria", "gorilla", "polonia", "arbol"};
     }
-   
+//contador lleno
+
+    public palabras(String[] listapalabras, String escogerpalabra, String palabraEcogida) {
+    String[] listaPalabras = null;
+        this.palabraEscogida = listaPalabras[numeroRandom];
+        this.listapalabras = listapalabras;
+        this.escogerpalabra = escogerpalabra;
+        this.palabraEscogida = palabraEcogida;
+    }
+//SETS Y GETS
+
+    /**
+     *Metodo get para la variable listapalabras
+     * @return
+     */
+    public String[] getListapalabras() {
+        return listapalabras;
+    }
+
+    /**
+     *Metodo set pars la variable listapalabras
+     * @param listapalabras
+     */
+    public void setListapalabras(String[] listapalabras) {
+        this.listapalabras = listapalabras;
+    }
+
+    /**
+     *Metodo get para la variable esogerPalabra
+     * @return
+     */
+    public String getEscogerpalabra() {
+        return escogerpalabra;
+    }
+
+    /**
+     *Metodo set para la variable escogerPalabra
+     * @param escogerpalabra
+     */
+    public void setEscogerpalabra(String escogerpalabra) {
+        this.escogerpalabra = escogerpalabra;
+    }
+
+    /**
+     *Metodo get para la variable palabraEscogida
+     * @return
+     */
+    public String getPalabraEcogida() {
+        return palabraEscogida;
+    }
+
+    /**
+     *metodo set para  variable palabraEscogida
+     * @param palabraEcogida
+     */
+    public void setPalabraEcogida(String palabraEcogida) {
+        this.palabraEscogida = palabraEcogida;
+    }
+
+    /**
+     *metodo get para la variable numeroRandom
+     * @return
+     */
+    public int getNumeroRandom() {
+        return numeroRandom;
+    }
+
+    /**
+     *Metodo set para variable numeroRandom
+     * @param numeroRandom
+     */
+    public void setNumeroRandom(int numeroRandom) {
+        this.numeroRandom = numeroRandom;
+    }
+
     
-   }
+    //metodos especificos
+    public String[] ListaPalabras(String[] listaPalabras){
+        String[] Listapalabras = listaPalabras; //Defino la variable a usar
+        Listapalabras = listapalabras; //Ejecuto el método
+        return Listapalabras; //Devuelvo la variable llena
+        }
+    
+        public String escogerPlabra(String escogerpalabra){
+        String escogerPalabra = escogerpalabra ; //Defino la variable a usar
+    String[] listaPalabras = null;
+         escogerPalabra = listaPalabras[numeroRandom];  //Ejecuto el método
+        return escogerPalabra; //Devuelvo la variable llena  
+    }
+     
+ 
+        
+    
+    
+    
     
     
     
 
+
+
+}
