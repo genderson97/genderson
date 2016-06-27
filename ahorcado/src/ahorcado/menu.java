0f8d8dfb3c199e423e10d0cb972110ea149a619b
genderson97 <gporrasm@ucatolica.ac.cr>
2016-06-26 22:21:23 -0600

    clases menu y palabras
    l

--- ahorcado/src/ahorcado/menu.java
+++ ahorcado/src/ahorcado/menu.java
@@ -1,34 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Paquetes de mi clase menu.
 package ahorcado;
-
-
+//Importes de mi clase Menu.
+import javax.swing.JOptionPane;
 /**
  *
  * @author gende
- */
-
-    
-
-    
-
-        
-        
-    
-    
-   
-
-            
-            
-            
-   
-
-    
-
-
-            
-    
-
+ **/
+public class Menu {
+    //Variable de tipo entero llamada opc.
+    int opc = Integer.parseInt(JOptionPane.showInputDialog("Presione 1 Para jugar, 2 para salir"));
+    //
+    int numrandom;
+    //
+    String[] listapalabras;
+    //
+    char letra;
+    //
+    String aleatoria;
+    //
+    int contador;
+    /**
+     * Menu de mi clase Menu.
+     **/
+    public void Menu() {
+        switch (opc) {
+            case 1:
+                Palabras p = new Palabras(numrandom, listapalabras, letra, aleatoria, contador);
+                p.Adivina();
+                break;
+            case 2:
+                System.exit(0);
+                break;
+        }
+    }
+}
