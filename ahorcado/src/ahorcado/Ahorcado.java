f811713b90edf27be8cfa9dc4ec1995a86e8a243
genderson97 <gporrasm@ucatolica.ac.cr>
2016-06-26 22:15:22 -0600

    ahorcado terminado falta terminar comentariado

--- ahorcado/src/ahorcado/Ahorcado.java
+++ ahorcado/src/ahorcado/Ahorcado.java
@@ -1,49 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Paquetes de mi Main
 package ahorcado;
-
-import java.util.Random;
-import java.util.Scanner;
-
 /**
  *
  * @author gende
- */
+ **/
 public class Ahorcado {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-                Scanner scanner = new Scanner(System.in);
-        String [] listaPalabras = {"avion","barco","computadora","dinosaurio","elefante","foca","gato","hielo","iguana","jabon"};
-        
-        //Elegimos un numero random, que sera el numero de palabra que legiremos de la lista de palabras
-        int numeroRandom = 1 + new Random().nextInt(listaPalabras.length);
-        
-        String palabraEscogida = listaPalabras[numeroRandom];//Guardamos la palabra escogida por la pc en una variable
-        System.out.println("Esta palabra tiene "+palabraEscogida.length()+" letras");
-        String[] palabra = new String[palabraEscogida.length()];//si se llena todos los espacios de este array es que ha ganado el juego
-        imprimirPalabra(palabra);//Mostramos las rayitas
-        int vidas = 11;//Contador de vidas
-        
-        
-        
-        
-        
-        
-        
-
-}
-
-    private static void imprimirPalabra(String[] palabra) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /**
+         * Instancio mi clase Menu.
+         **/
+        Menu mimenu = new Menu();
+        /*
+        * Llamo a mi clase Menu.
+        **/
+        mimenu.Menu();
     }
-     
-    
-  }
-    
-
+}
