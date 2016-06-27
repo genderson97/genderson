927cb30827a2924e5d9465a3e09dc69f2e5813d0
genderson97 <gporrasm@ucatolica.ac.cr>
2016-06-26 22:24:23 -0600

    clase palabras
    los hice seguidos porque no los marque todos y no me fije bien

--- ahorcado/src/ahorcado/palabras.java
+++ ahorcado/src/ahorcado/palabras.java
@@ -1,126 +1,128 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Paquetes de mi Clase Palabras.
 package ahorcado;
-
-import java.util.Random;
-
+//Importes de mi Clase Palabras.
+import static java.lang.Character.toLowerCase;
+import java.util.Arrays;
+import javax.swing.JOptionPane;
 /**
+ * @author Genderson Porras
+ * @date ***
  *
- * @author gende
  */
-public class palabras {
-private String[] listapalabras;        
-private String escogerpalabra;
-private String palabraEscogida; ;
-private int numeroRandom = 1+ new Random().nextInt(listapalabras.length);    
-//contador vacio
-    public palabras() {
-    String[] listaPalabras = null;
-        this.palabraEscogida = listaPalabras[numeroRandom];
-        this.listapalabras = new String[]{"teleferico", "televisor", "computador", "coche", "antropologia", "rusia", "hungria", "gorilla", "polonia", "arbol"};
-    }
-//contador lleno
+public class Palabras {
 
-    public palabras(String[] listapalabras, String escogerpalabra, String palabraEcogida) {
-    String[] listaPalabras = null;
-        this.palabraEscogida = listaPalabras[numeroRandom];
-        this.listapalabras = listapalabras;
-        this.escogerpalabra = escogerpalabra;
-        this.palabraEscogida = palabraEcogida;
-    }
-//SETS Y GETS
-
+    //Arreglo de tipo String llamado listapalabras.
+    String[] listapalabras;
+    char letra;
+    String aleatoria;
+    int numrandom;
+    int contador;
     /**
-     *Metodo get para la variable listapalabras
-     * @return
+     * Constructor vacio.
+     *
      */
+    public Palabras() {
+    }
+    /**
+     * Constructor de mi clase Palabras.
+     *
+     * @param listapalabras
+     * @param letra
+     * @param aleatoria
+     *
+     */
+    public Palabras(int numrandom, String[] listapalabras, char letra, String aleatoria, int contador) {
+        this.listapalabras = new String[10];
+        this.listapalabras[0] = "yuca";
+        this.listapalabras[1] = "gato";
+        this.listapalabras[2] = "tren";
+        this.listapalabras[3] = "arbol";
+        this.listapalabras[4] = "auto";
+        this.listapalabras[5] = "clavo";
+        this.listapalabras[6] = "nido";
+        this.listapalabras[7] = "trapo";
+        this.listapalabras[8] = "tanque";
+        this.listapalabras[9] = "juan";
+        this.letra = letra;
+        this.numrandom = (int) (Math.random() * 10);
+        this.aleatoria = this.listapalabras[this.numrandom];
+        this.contador = 11;
+    }
+    /**
+     * Metodo get de mi arreglo listapalabras.
+     * @return listapalabras. 
+     **/
     public String[] getListapalabras() {
         return listapalabras;
     }
-
-    /**
-     *Metodo set pars la variable listapalabras
-     * @param listapalabras
-     */
+    
     public void setListapalabras(String[] listapalabras) {
         this.listapalabras = listapalabras;
     }
-
     /**
-     *Metodo get para la variable esogerPalabra
-     * @return
-     */
-    public String getEscogerpalabra() {
-        return escogerpalabra;
+     * Metodo get de mi variable letra.
+     * @return letra
+     **/
+    public char getLetra() {
+        return letra;
     }
 
-    /**
-     *Metodo set para la variable escogerPalabra
-     * @param escogerpalabra
-     */
-    public void setEscogerpalabra(String escogerpalabra) {
-        this.escogerpalabra = escogerpalabra;
+    public void setLetra(char letra) {
+        this.letra = letra;
     }
 
-    /**
-     *Metodo get para la variable palabraEscogida
-     * @return
-     */
-    public String getPalabraEcogida() {
-        return palabraEscogida;
+    public String getAleatoria() {
+        return aleatoria;
     }
 
-    /**
-     *metodo set para  variable palabraEscogida
-     * @param palabraEcogida
-     */
-    public void setPalabraEcogida(String palabraEcogida) {
-        this.palabraEscogida = palabraEcogida;
+    public void setAleatoria(String aleatoria) {
+        this.aleatoria = aleatoria;
     }
 
-    /**
-     *metodo get para la variable numeroRandom
-     * @return
-     */
-    public int getNumeroRandom() {
-        return numeroRandom;
+    public int getNumrandom() {
+        return numrandom;
     }
 
-    /**
-     *Metodo set para variable numeroRandom
-     * @param numeroRandom
-     */
-    public void setNumeroRandom(int numeroRandom) {
-        this.numeroRandom = numeroRandom;
+    public void setNumrandom(int numrandom) {
+        this.numrandom = numrandom;
     }
 
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
     
-    //metodos especificos
-    public String[] ListaPalabras(String[] listaPalabras){
-        String[] Listapalabras = listaPalabras; //Defino la variable a usar
-        Listapalabras = listapalabras; //Ejecuto el método
-        return Listapalabras; //Devuelvo la variable llena
+    public void Adivina() {
+        System.out.println("Usted posee " + contador + " intentos");//mensaje de cuantos intentos tiene
+        char oculta[] = getAleatoria().toCharArray();//la palabra va aparecer en un arreglo de caracteres
+        for (int i = 0; i < getAleatoria().length(); i++) {//ciclo for para get aleatoria rellena la palabra con esos caracteres
+                oculta[i] = '_';//caracter oculto va hacer igual a esecaracter
+            }       
+        System.out.println(Arrays.toString(oculta));
+        while (contador != 0) {
+            if (!(Arrays.toString(oculta).contains("_"))) {
+                JOptionPane.showMessageDialog(null, "You Winner", "Fin", JOptionPane.WARNING_MESSAGE);
+                break;
+            }
+            //Carga el primer caracter en la variable letra y la trasforma a minuscula.
+            setLetra(toLowerCase(JOptionPane.showInputDialog("Ingrese una letra").charAt(0)));
+            if (getAleatoria().contains(String.valueOf(letra))) {
+                int index = getAleatoria().indexOf(letra);
+                oculta[index] = letra;
+                System.out.println(Arrays.toString(oculta));
+            } else {
+                JOptionPane.showMessageDialog(null, "Letra Incorrecta, Intente de nuevo", "ERROR", JOptionPane.ERROR_MESSAGE);//imprime un aviso que la letra que se inserto esta incoreccta
+                contador = contador - 1;//se descontara un intento
+                System.out.println("Tienes: "+contador+" intentos restantes");//mensaje de cuantos intentos le quedan
+            }
         }
-    
-        public String escogerPlabra(String escogerpalabra){
-        String escogerPalabra = escogerpalabra ; //Defino la variable a usar
-    String[] listaPalabras = null;
-         escogerPalabra = listaPalabras[numeroRandom];  //Ejecuto el método
-        return escogerPalabra; //Devuelvo la variable llena  
+        if (contador == 0) {//si el contador llega a cero
+             JOptionPane.showMessageDialog(null, "Game Over", "Fin", JOptionPane.ERROR_MESSAGE);//imprime que ha perdido
+        }
+        Menu m = new Menu();//menu para reiniciar o salir del juego
+        m.Menu();
     }
-     
- 
-        
-    
-    
-    
-    
-    
-    
-
-
-
 }
